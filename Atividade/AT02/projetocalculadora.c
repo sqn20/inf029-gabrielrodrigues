47d4cd7867ff0171ed2ddbf8c4b15f1d3f13164d
sqn20 <gabrielrf2050@gmail.com>
2021-08-21 19:22:26 -0300

    AT02

---
+++ Atividade/AT02/projetocalculadora.c
@@ -0,0 +1,11 @@
+//projeto calculadora.c 
+
+#include <stdio.h> 
+int main (){	
+	int opcaoperacao; 
+	printf ("Digite a Opção de Operação:\n");
+	printf ("0 - Sair, 1- Soma, 2- Subtração, 3- Multiplicar, 4-Dividir\n"); 
+	scanf ("%d",&opcaoperacao); 
+
+	return 0; 
+}
